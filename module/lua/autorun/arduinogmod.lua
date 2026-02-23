dd790dfc3ecd1bf70c1ee12baa3a27c396bcc41c
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2026-02-23 18:42:48 -0500

    Simplify module name

--- module/lua/autorun/arduinogmod.lua
+++ module/lua/autorun/arduinogmod.lua
@@ -1,10 +1,10 @@
 --Safely initialize the module
-local function ArduinoGmod_Init()
-	if !util.IsBinaryModuleInstalled( "arduinogmod" ) then
-		MsgC( Color( 255, 0, 0 ), "\nERROR: ArduinoGmod module not found.\n" )
+local version = "1.3"
+hook.Add( "InitPostEntity", "ArduinoModuleInit", function()
+	if !util.IsBinaryModuleInstalled( "arduino" ) then
+		MsgC( Color( 255, 0, 0 ), "\nERROR: Arduino module not found.\n" )
 		return
 	end
-	require( "arduinogmod" )
-	MsgC( Color( 0, 255, 255 ), "\nArduinoGmod module successfully loaded.\n" )
-end
-hook.Add( "InitPostEntity", "ArduinoGmodInit", ArduinoGmod_Init )
+	require( "arduino" )
+	MsgC( Color( 0, 255, 255 ), "\nArduino module v"..version.." successfully loaded.\n" )
+end )
